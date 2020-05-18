067c18ca5d9e794d36769b0523c1f071aeb0fb2b
Surfsol <surfsol@protonamil.com>
2020-05-18 17:36:34 -0300

    Pin.js add handleClick

--- src/Components/BubbleMap/Pin.js
+++ src/Components/BubbleMap/Pin.js
@@ -1,9 +1,15 @@
-import React from "react";
-import { Marker } from "react-map-gl";
+import React, { useState } from "react";
+import { Marker, Popup } from "react-map-gl";
 import circle from "./images/circle.svg";
 
 const Pin = ({ markets }) => {
   console.log(markets.features);
+  const [selectedMkt, setSelectedMkt] = useState({});
+  console.log(selectedMkt);
+
+  const handleClick = e => {
+    setSelectedMkt(e);
+  };
 
   return (
     <>
@@ -14,11 +20,27 @@ const Pin = ({ markets }) => {
         return (
           <Marker key={e} latitude={coordinates[0]} longitude={coordinates[1]}>
             <img
-              //onClick={() => handleClick(sensor)}
+              onClick={() => handleClick(e)}
               className="circle"
               src={circle}
               alt="location"
             />
+            {selectedMkt !== "" ? (
+              <Popup
+                className="popupCard"
+                latitude={e.geometry.coordinates[0]}
+                longitude={e.geometry.coordinates[1]}
+                onClose={() => {
+                  handleClick("");
+                }}
+                //closeOnClick={false}
+              >
+                <h1>{e.properties.description}</h1>
+                {/* <PopupInfo
+               description={e.properties.description}
+              /> */}
+              </Popup>
+            ) : null}
           </Marker>
         );
       })}
